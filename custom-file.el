dc25615634431624e9f5682ce9caca2cf7373bab
Hao He <hehao9504@gmail.com>
2020-05-05 13:33:12 +0800

    修改README.md，添加custom-file.el

---
+++ custom-file.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (htmlize which-key use-package toggle-test spacemacs-theme region-bindings-mode projectile paredit org-superstar org-bullets nord-theme multiple-cursors magit ivy-posframe ido-completing-read+ highlight-symbol git-timemachine git-gutter flycheck-joker flycheck-clj-kondo exec-path-from-shell esup dumb-jump diminish delight define-word counsel company-emoji cnfonts clojure-mode-extra-font-locking cider centaur-tabs aggressive-indent ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.6 :foreground "#FD971F"))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.4 :foreground "#A6E22E"))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2 :foreground "#66D9EF"))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0 :foreground "#E6DB74"))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0 :foreground "#A1EFE4"))))
+ '(org-level-6 ((t (:inherit outline-6 :height 1.0 :foreground "#A6E22E"))))
+ '(org-level-7 ((t (:inherit outline-7 :height 1.0 :foreground "#F92672"))))
+ '(org-level-8 ((t (:inherit outline-8 :height 1.0 :foreground "#66D9EF")))))
